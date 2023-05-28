1f06b64e75e9853e8da6c4a5edd880eae277a730
Cecilia Torreira <celia.umbreon@gmail.com>
2023-05-28 17:21:29 +0100

    Creadas primeras páginas y Navbar con links funcionales

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      
+      <Link to="/beers">All Beers</Link>
+      <br />
+
+      <Link to="/random">Random Beer</Link>
+
+      <br />
+      <Link to="/newbeer">New Beer</Link>
+    </div>
+  );
+}
+
+export default Home;
